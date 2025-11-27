100dc1d028a14fc1812f58c71042f7b717cc68b5
Pedro Lubacheski <70827056+pedrolubacheski@users.noreply.github.com>
2025-11-27 14:26:20 -0300

    Commit 13
    Commit 13

--- ChildrensPrice.java
+++ ChildrensPrice.java
@@ -3,3 +3,17 @@ public class ChildrensPrice extends Price {
         return Movie.CHILDRENS;
     }
 }
+
+public class ChildrensPrice extends Price {
+    public int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+
+    public double getCharge(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+    }
+}
+
--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -3,3 +3,13 @@ public class NewReleasePrice extends Price {
         return Movie.NEW_RELEASE;
     }
 }
+
+public class NewReleasePrice extends Price {
+    public int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
+}
