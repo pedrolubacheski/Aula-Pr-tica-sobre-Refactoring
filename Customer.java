f89317726e61ffafe382eefff198a77528a01ed6
Pedro Lubacheski <70827056+pedrolubacheski@users.noreply.github.com>
2025-11-27 14:15:16 -0300

    Commit 7
    Commit 7

--- Customer.java
+++ Customer.java
@@ -17,24 +17,46 @@ public class Customer {
       return _name;
    }
 
-   public String statement() {
-      double totalAmount = 0;
-      int frequentRenterPoints = 0;
-      Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
+public String statement() {
+    Enumeration rentals = _rentals.elements();
+    String result = "Rental Record for " + getName() + "\n";
 
-      while (rentals.hasMoreElements()) {
-    Rental each = (Rental) rentals.nextElement();
+    while (rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
 
-    // add frequent renter points (agora usando o método movido)
-    frequentRenterPoints += each.getFrequentRenterPoints();
+        // show figures for this rental
+        result += "\t" + each.getMovie().getTitle() + "\t" +
+                  String.valueOf(each.getCharge()) + "\n";
+    }
 
-    // show figures for this rental
-    result += "\t" + each.getMovie().getTitle() + "\t" +
-              String.valueOf(each.getCharge()) + "\n";
+    // add footer lines
+    result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+    result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
+              " frequent renter points";
 
-    totalAmount += each.getCharge();
+    return result;
 }
+
+    private double getTotalCharge() {
+    double result = 0;
+    Enumeration rentals = _rentals.elements();
+    while (rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        result += each.getCharge();
+    }
+    return result;
+}
+
+    private int getTotalFrequentRenterPoints() {
+    int result = 0;
+    Enumeration rentals = _rentals.elements();
+    while (rentals.hasMoreElements()) {
+        Rental each = (Rental) rentals.nextElement();
+        result += each.getFrequentRenterPoints();
+    }
+    return result;
+}
+
 
 
       // add footer lines
@@ -44,6 +66,4 @@ public class Customer {
       return result;
    }
 
-      return result;
-   }
 
