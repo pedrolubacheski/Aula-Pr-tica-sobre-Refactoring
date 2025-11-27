03c8656f3f9476030070fbfc078878029b4b3b26
Pedro Lubacheski <70827056+pedrolubacheski@users.noreply.github.com>
2025-11-27 14:29:41 -0300

    Commit 15
    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -2,14 +2,13 @@ public class NewReleasePrice extends Price {
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
-}
-
-public class NewReleasePrice extends Price {
-    public int getPriceCode() {
-        return Movie.NEW_RELEASE;
-    }
 
     public double getCharge(int daysRented) {
         return daysRented * 3;
+    }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2 : 1;
     }
 }
--- Price.java
+++ Price.java
@@ -5,10 +5,7 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) &&
-            daysRented > 1) {
-            return 2;
-        }
-        return 1;
-    }
+    return 1; // comportamento padrão
+}
+
 }
