6b7867956e5021776d01517173cf8b24957aa0c6
Pedro Lubacheski <70827056+pedrolubacheski@users.noreply.github.com>
2025-11-27 14:18:56 -0300

    Commit 9
    Commit 9

--- Rental.java
+++ Rental.java
@@ -17,36 +17,7 @@ public class Rental {
    }
 
    public double getCharge() {
-    double result = 0;
-
-    switch (this.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-            result += 2;
-            if (this.getDaysRented() > 2)
-                result += (this.getDaysRented() - 2) * 1.5;
-            break;
-
-        case Movie.NEW_RELEASE:
-            result += this.getDaysRented() * 3;
-            break;
-
-        case Movie.CHILDRENS:
-            result += 1.5;
-            if (this.getDaysRented() > 3)
-                result += (this.getDaysRented() - 3) * 1.5;
-            break;
-    }
-
-    public int getFrequentRenterPoints() {
-    if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-        this.getDaysRented() > 1) {
-        return 2;
-    }
-    return 1;
-}
-
-
-    return result;
+    return _movie.getCharge(_daysRented);
 }
 
 }
